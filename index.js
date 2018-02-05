d5fb72e4960c3f34dde06089fe70fe77c391dbb1
Jose Ruy Borges <jrbpfilho@gmail.com>
2018-02-05 15:12:26 -0200

    Feeding the DB for testing

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const mysql = require('mysql2/promise')
 
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
@@ -8,15 +9,17 @@ app.get('/', (req, res) => {
     res.render('home')
 })
 
-const mysql = require('mysql2/promise')
-const connection = mysql.createConnection({
-    host: '127.0.0.1',
-    user: 'root',
-    password: 'van23ruy',
-    database: 'futiba-club'
-}).then( conn => {
-    console.log(conn)
+const init = async() => {
+    const connection = await mysql.createConnection({
+        host: '127.0.0.1',
+        user: 'root',
+        password: 'van23ruy',
+        database: 'futibaclub'
 })
+const [rows, fields] = await connection.execute('select * from users')
+console.log(rows)
+}
+init()
 
 app.listen(3000, err => {
     console.log('Futiba Club server is running...')
