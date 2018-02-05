7f767c63a465d82045c0c7cc4cfd6b64407542a9
Jose Ruy Borges <jrbpfilho@gmail.com>
2018-02-05 15:03:25 -0200

    Initializing Futiba-Club DB

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const express = require('express')
 const app = express()
 
+app.use(express.static('public'))
+app.set('view engine', 'ejs')
+
 app.get('/', (req, res) => {
-    res.send('Olá Futiba Club')
+    res.render('home')
+})
+
+const mysql = require('mysql2/promise')
+const connection = mysql.createConnection({
+    host: '127.0.0.1',
+    user: 'root',
+    password: 'van23ruy',
+    database: 'futiba-club'
+}).then( conn => {
+    console.log(conn)
 })
 
 app.listen(3000, err => {
