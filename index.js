bc477dc2f798442a82c7f9d84882bc52ee0e9c6a
Jose Ruy Borges <jrbpfilho@gmail.com>
2018-02-09 23:47:22 -0200

    Heroku changes2

--- index.js
+++ index.js
@@ -16,15 +16,23 @@ app.use(session({
 }))
 app.set('view engine', 'ejs')
 
+// Local DB
 
-
-const init = async() => {
+/* const init = async() => {
     const connection = await mysql.createConnection({
         host: '127.0.0.1',
         user: 'root',
         password: 'van23ruy',
         database: 'futibaclub'
-    })
+    }) */
+
+    const init = async() => {
+        const connection = await mysql.createConnection({
+            host: 'us-cdbr-iron-east-05.cleardb.net',
+            user: 'b6ea0c712ef891',
+            password: '9d3a2b9a21b3ddf',
+            database: 'heroku_4500ee00b4b4ec5'
+        })
 
     app.use((req, res, next) => {
         if(req.session.user) {
